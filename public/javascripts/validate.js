b458cf583d611714f76d3df1b61e5dfb7786ce53
snoryder8019 <snoryder8019@gmail.com>
2022-06-28 13:34:22 -0600

    git stuff

--- public/javascripts/validate.js
+++ public/javascripts/validate.js
@@ -6,14 +6,20 @@ const regSubmit = document.getElementById('regSubmit');
 const regForm = [regName,regEmail,regPass,regBox];
 const regValidateError = document.getElementById('regValidateError');
 
+
+function annie1(id,opacity,transY){
+    id.style.opacity=opacity;
+    id.style.transform="translateY("+transY+"px)"
+}
 //this checks all fields are filled out and updates the button and validate error message
 function validateFill(){
     for(i=0;i<regForm.length;i++){
     regForm[i].addEventListener("input", function(){
      if (regName.value==="" || regEmail.value==="" || regPass.value==="" || regBox.checked===false){
          console.log('its null')
-         regValidateError.style.opacity=1;
-         regValidateError.style.transform="translateY(0px)"
+         annie1(regValidateError,1,0);
+       //  regValidateError.style.opacity=1;
+        // regValidateError.style.transform="translateY(0px)"
          regSubmit.disabled=true;
      }else{
          console.log('not Null');
