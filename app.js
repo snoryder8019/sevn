f533349cdf20c6965348d1bbb8e30a23af70d647
snoryder8019 <snoryder8019@gmail.com>
2022-07-17 22:18:55 -0600

    api added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 var adminRouter =require('./routes/admin');
+var apiRouter =require('./routes/api');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(postsRouter);
 app.use(adminRouter);
+app.use(apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
